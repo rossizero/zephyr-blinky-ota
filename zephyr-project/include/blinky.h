c90c795399e388a47772bdf6c7ac50ffa4010939
Sebastian Rossi <sebastian.rossi@maibornwolff.de>
2025-07-08 11:29:03 +0200

    simple blinky works

--- zephyr-project/include/blinky.h
+++ zephyr-project/include/blinky.h
@@ -3,40 +3,8 @@
 
 #include <zephyr/kernel.h>
 
-typedef enum {
-    LED_MODE_OFF,
-    LED_MODE_ON,
-    LED_MODE_BLINK_NORMAL,
-    LED_MODE_BLINK_FAST,
-    LED_MODE_BLINK_SLOW
-} led_mode_t;
-
-typedef enum {
-    LED_STATUS_IDLE,
-    LED_STATUS_WIFI_CONNECTING,
-    LED_STATUS_WIFI_CONNECTED,
-    LED_STATUS_OTA_CHECKING,
-    LED_STATUS_OTA_DOWNLOADING,
-    LED_STATUS_ERROR
-} led_status_t;
-
-/**
- * @brief Set LED mode
- * @param mode LED mode to set
- */
-void led_set_mode(led_mode_t mode);
-
-/**
- * @brief Set LED status (changes mode automatically)
- * @param status Application status
- */
-void led_set_status(led_status_t status);
-
-/**
- * @brief Get current LED mode
- * @return Current LED mode
- */
-led_mode_t led_get_mode(void);
+/* Blink interval in milliseconds */
+#define BLINK_INTERVAL_MS 1000
 
 /**
  * @brief Initialize LED subsystem
--- zephyr-project/src/blinky.c
+++ zephyr-project/src/blinky.c
@@ -4,7 +4,6 @@
 #include <zephyr/logging/log.h>
 
 #include "blinky.h"
-#include "app_config.h"
 
 LOG_MODULE_REGISTER(blinky, LOG_LEVEL_INF);
 
@@ -13,108 +12,18 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 /* Work item for LED control */
 static struct k_work_delayable blink_work;
-
-/* LED state */
-static struct {
-    led_mode_t current_mode;
-    led_status_t current_status;
-    uint8_t led_state;
-    uint32_t blink_interval;
-} led_ctx = {
-    .current_mode = LED_MODE_BLINK_NORMAL,
-    .current_status = LED_STATUS_IDLE,
-    .led_state = 0,
-    .blink_interval = 1000
-};
+static uint8_t led_state = 0;
 
 static void blink_work_handler(struct k_work *work)
 {
-    switch (led_ctx.current_mode) {
-    case LED_MODE_OFF:
-        gpio_pin_set_dt(&led, 0);
-        /* Don't reschedule for OFF mode */
-        return;
-        
-    case LED_MODE_ON:
-        gpio_pin_set_dt(&led, 1);
-        /* Don't reschedule for ON mode */
-        return;
-        
-    case LED_MODE_BLINK_NORMAL:
-    case LED_MODE_BLINK_FAST:
-    case LED_MODE_BLINK_SLOW:
-        /* Toggle LED */
-        gpio_pin_set_dt(&led, led_ctx.led_state);
-        led_ctx.led_state ^= 1;
-        
-        /* Reschedule with current interval */
-        k_work_schedule(&blink_work, K_MSEC(led_ctx.blink_interval));
-        break;
-    }
-}
-
-static void update_blink_interval(void)
-{
-    switch (led_ctx.current_mode) {
-    case LED_MODE_BLINK_FAST:
-        led_ctx.blink_interval = LED_FAST_BLINK_MS;
-        break;
-    case LED_MODE_BLINK_NORMAL:
-        led_ctx.blink_interval = LED_BLINK_INTERVAL_MS;
-        break;
-    case LED_MODE_BLINK_SLOW:
-        led_ctx.blink_interval = LED_SLOW_BLINK_MS;
-        break;
-    default:
-        led_ctx.blink_interval = LED_BLINK_INTERVAL_MS;
-        break;
-    }
-}
-
-void led_set_mode(led_mode_t mode)
-{
-    if (led_ctx.current_mode != mode) {
-        led_ctx.current_mode = mode;
-        update_blink_interval();
-        
-        /* Cancel current work and start new one */
-        k_work_cancel_delayable(&blink_work);
-        k_work_schedule(&blink_work, K_NO_WAIT);
-    }
-}
-
-void led_set_status(led_status_t status)
-{
-    if (led_ctx.current_status != status) {
-        led_ctx.current_status = status;
-        
-        /* Map status to LED mode */
-        switch (status) {
-        case LED_STATUS_IDLE:
-            led_set_mode(LED_MODE_BLINK_SLOW);
-            break;
-        case LED_STATUS_WIFI_CONNECTING:
-            led_set_mode(LED_MODE_BLINK_FAST);
-            break;
-        case LED_STATUS_WIFI_CONNECTED:
-            led_set_mode(LED_MODE_BLINK_NORMAL);
-            break;
-        case LED_STATUS_OTA_CHECKING:
-            led_set_mode(LED_MODE_BLINK_NORMAL);
-            break;
-        case LED_STATUS_OTA_DOWNLOADING:
-            led_set_mode(LED_MODE_BLINK_FAST);
-            break;
-        case LED_STATUS_ERROR:
-            led_set_mode(LED_MODE_ON);
-            break;
-        }
-    }
-}
-
-led_mode_t led_get_mode(void)
-{
-    return led_ctx.current_mode;
+    ARG_UNUSED(work);
+    
+    /* Toggle LED */
+    gpio_pin_set_dt(&led, led_state);
+    led_state ^= 1;
+    
+    /* Reschedule with fixed interval */
+    k_work_schedule(&blink_work, K_MSEC(BLINK_INTERVAL_MS));
 }
 
 int blinky_init(void)
@@ -133,10 +42,10 @@ int blinky_init(void)
     /* Initialize work */
     k_work_init_delayable(&blink_work, blink_work_handler);
     
-    /* Start with idle status */
-    led_set_status(LED_STATUS_IDLE);
+    /* Start blinking */
+    k_work_schedule(&blink_work, K_MSEC(BLINK_INTERVAL_MS));
 
-    LOG_INF("Blinky subsystem initialized");
+    LOG_INF("Blinky subsystem initialized - blinking every %d ms", BLINK_INTERVAL_MS);
     return 0;
 }
 
